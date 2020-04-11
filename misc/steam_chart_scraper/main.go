53ac5e490d0ea9fd6637cc82462ad18870efb7c6
J-leg <jamesz96.au@gmail.com>
2020-04-11 22:17:21 +1000

    Add license + gitignore + editor config

--- misc/steam_chart_scraper/main.go
+++ misc/steam_chart_scraper/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 
 	// Logs
-	f, err := os.OpenFile("logs.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	f, err := os.OpenFile("logs.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("error opening file: %v", err)
 	}
