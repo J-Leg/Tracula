0cba867541801ec9026409bd025c936ef9a6bdf3
J-leg <jamesz96.au@gmail.com>
2020-04-13 16:47:35 +1000

    Implement monthly process (averaging + cleaning + sorting)

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 )
 
 func main() {
+	executeMonthly()
 	execute()
 	recover()
 }
@@ -31,7 +32,37 @@ func recover() {
 	fmt.Printf("Total elapsed recovery time: %s\n\n", recoveryElapsed.String())
 }
 
+func executeMonthly() {
+
+	f, err := os.OpenFile("logs/exec_monthly.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	log.SetOutput(f)
+
+	// First day of the month
+	if time.Now().Day() == 1 {
+		var executionElapsed time.Duration
+		var start time.Time
+		var dStart time.Time
+
+		fmt.Println("~~~~~~~ Execute Monthly Update ~~~~~~~")
+		start = time.Now()
+		core.ExecuteMonthly()
+		dStart = time.Now()
+		fmt.Println("~~~~~~~ Monthly Update Complete ~~~~~~~")
+		f.Close()
+		executionElapsed = dStart.Sub(start)
+
+		fmt.Printf("Total elapsed (Monthly) execution time: %s\n\n", executionElapsed.String())
+	}
+}
+
 func execute() {
+	var executionElapsed time.Duration
+	var start time.Time
+	var dStart time.Time
+
 	f, err := os.OpenFile("logs/exec.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("error opening file: %v", err)
@@ -39,12 +70,12 @@ func execute() {
 	log.SetOutput(f)
 
 	fmt.Println("~~~~~~~ Execute Daily Update ~~~~~~~")
-	start := time.Now()
+	start = time.Now()
 	core.Execute()
-	dStart := time.Now()
+	dStart = time.Now()
 	fmt.Println("~~~~~~~ Daily Update Complete ~~~~~~~")
 	f.Close()
-	executionElapsed := dStart.Sub(start)
+	executionElapsed = dStart.Sub(start)
 
-	fmt.Printf("Total elapsed execution time: %s\n\n", executionElapsed.String())
+	fmt.Printf("Total elapsed (Daily) execution time: %s\n\n", executionElapsed.String())
 }
